80c6c695c4e7a961f93a1eb682c845d32c6e5fea
陈海洋 <1559843332@qq.com>
2020-03-05 19:23:48 +0800

    设置高亮显示

--- src/main/java/cn/chendahai/controller/QueryController.java
+++ src/main/java/cn/chendahai/controller/QueryController.java
@@ -2,6 +2,8 @@ package cn.chendahai.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.search.SearchRequest;
@@ -13,6 +15,7 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,11 +38,6 @@ public class QueryController {
 
     /**
      * 根据id获取信息
-     * @param index
-     * @param type
-     * @param id
-     * @return
-     * @throws IOException
      */
     @GetMapping("getById")
     public ResponseEntity getById(String index, String type, Integer id) throws IOException {
@@ -52,11 +50,6 @@ public class QueryController {
 
     /**
      * 硬编码查询
-     * @param index
-     * @param type
-     * @param name
-     * @return
-     * @throws IOException
      */
     @GetMapping("search")
     public ResponseEntity search(String index, String type, String name) throws IOException {
@@ -70,7 +63,17 @@ public class QueryController {
         searchSourceBuilder.size(20);
 
         // 第一个是获取字段，第二个是过滤的字段，默认获取全部
-        searchSourceBuilder.fetchSource(new String[]{"id", "name", "age"}, new String[]{"name"});
+//        searchSourceBuilder.fetchSource(new String[]{"id", "name", "age"}, new String[]{"name"});
+
+        List<String> highlightFieldList = new ArrayList<>();
+        highlightFieldList.add("name");
+        highlightFieldList.add("scheduledDate");
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        for (int x = 0; x < highlightFieldList.size(); x++) {
+            HighlightBuilder.Field field = new HighlightBuilder.Field(highlightFieldList.get(x)).preTags("<high>").postTags("</high>");
+            highlightBuilder.field(field);
+        }
+        searchSourceBuilder.highlighter(highlightBuilder);
 
         SearchRequest request = new SearchRequest(index);
         request.types(type);
